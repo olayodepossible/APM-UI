2fd4c1f25837d35ece8652888df28078ca87e235
Akin <>
2022-07-22 14:49:51 +0100

    implement ProductList interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'APM-UI';
+  pageTitle = 'APM Product Management';
 }
